7efbcd2cb3fde95cdabaceac7ef509e33b23a399
Michael Stromer <>
2020-06-23 23:05:05 -0400

    find books

--- src/lib/resolvers.ts
+++ src/lib/resolvers.ts
@@ -10,6 +10,9 @@ const Query: Required<QueryResolvers<ResolverContext>> = {
   book(_parent, args, _context, _info): BookNode {
     const { id } = args;
     const { node } = books.edges.find((book) => book.node.id === id);
+    if (!node) {
+      throw new Error("Book Not Found") 
+    }
     return node;
   },
   books(_parent, _args, _context, _info): BookNodeConnection {
